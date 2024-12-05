d7efeb3836b063e58e3ab8eabbe63914b9f8625d
Isabela Costa <102261435+isabelaCosta95@users.noreply.github.com>
2024-12-05 15:40:37 -0300

    Create paradaDAO.php

---
+++ tcc/DAO/paradaDAO.php
@@ -0,0 +1,22 @@
+<?php
+class ParadaDAO {
+    private $conexao;
+
+    public function __construct() {
+        include_once "MySQL.php";
+        $this->conexao = new MySQL();
+    }
+
+    public function insert($dados_parada) {
+        $sql = "INSERT INTO parada (parada, local, valor_unitario, id_viagem) 
+                VALUES (?, ?, ?, ?)";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $dados_parada['parada']);
+        $stmt->bindValue(2, $dados_parada['local']);
+        $stmt->bindValue(3, $dados_parada['valor_unitario']);
+        $stmt->bindValue(4, $dados_parada['id_viagem']);
+
+        $stmt->execute();
+    }
+}
